0bd72ca4a36963481dedd0244722f978b8fbf557
pc-rachat <noreply@example.com>
2026-08-15 18:05:15 +0200

    feat: aggregate CPU/GPU search functions across all 7 retailers

--- retailers/__init__.py
+++ retailers/__init__.py
@@ -1,24 +1,38 @@
 from retailers.ldlc import search_prices as ldlc_search
 from retailers.ldlc import search_ram_prices as ldlc_ram_search
 from retailers.ldlc import search_storage_prices as ldlc_storage_search
+from retailers.ldlc import search_cpu_prices as ldlc_cpu_search
+from retailers.ldlc import search_gpu_prices as ldlc_gpu_search
 from retailers.pccomponentes import search_prices as pccomponentes_search
 from retailers.pccomponentes import search_ram_prices as pccomponentes_ram_search
 from retailers.pccomponentes import search_storage_prices as pccomponentes_storage_search
+from retailers.pccomponentes import search_cpu_prices as pccomponentes_cpu_search
+from retailers.pccomponentes import search_gpu_prices as pccomponentes_gpu_search
 from retailers.materiel_net import search_prices as materiel_net_search
 from retailers.materiel_net import search_ram_prices as materiel_net_ram_search
 from retailers.materiel_net import search_storage_prices as materiel_net_storage_search
+from retailers.materiel_net import search_cpu_prices as materiel_net_cpu_search
+from retailers.materiel_net import search_gpu_prices as materiel_net_gpu_search
 from retailers.topachat import search_prices as topachat_search
 from retailers.topachat import search_ram_prices as topachat_ram_search
 from retailers.topachat import search_storage_prices as topachat_storage_search
+from retailers.topachat import search_cpu_prices as topachat_cpu_search
+from retailers.topachat import search_gpu_prices as topachat_gpu_search
 from retailers.grosbill import search_prices as grosbill_search
 from retailers.grosbill import search_ram_prices as grosbill_ram_search
 from retailers.grosbill import search_storage_prices as grosbill_storage_search
+from retailers.grosbill import search_cpu_prices as grosbill_cpu_search
+from retailers.grosbill import search_gpu_prices as grosbill_gpu_search
 from retailers.rueducommerce import search_prices as rueducommerce_search
 from retailers.rueducommerce import search_ram_prices as rueducommerce_ram_search
 from retailers.rueducommerce import search_storage_prices as rueducommerce_storage_search
+from retailers.rueducommerce import search_cpu_prices as rueducommerce_cpu_search
+from retailers.rueducommerce import search_gpu_prices as rueducommerce_gpu_search
 from retailers.amazon import search_prices as amazon_search
 from retailers.amazon import search_ram_prices as amazon_ram_search
 from retailers.amazon import search_storage_prices as amazon_storage_search
+from retailers.amazon import search_cpu_prices as amazon_cpu_search
+from retailers.amazon import search_gpu_prices as amazon_gpu_search
 
 ALL_SEARCH_FUNCTIONS = [
     ldlc_search,
@@ -48,4 +62,24 @@ ALL_STORAGE_SEARCH_FUNCTIONS = [
     grosbill_storage_search,
     rueducommerce_storage_search,
     amazon_storage_search,
+]
+
+ALL_CPU_SEARCH_FUNCTIONS = [
+    ldlc_cpu_search,
+    pccomponentes_cpu_search,
+    materiel_net_cpu_search,
+    topachat_cpu_search,
+    grosbill_cpu_search,
+    rueducommerce_cpu_search,
+    amazon_cpu_search,
+]
+
+ALL_GPU_SEARCH_FUNCTIONS = [
+    ldlc_gpu_search,
+    pccomponentes_gpu_search,
+    materiel_net_gpu_search,
+    topachat_gpu_search,
+    grosbill_gpu_search,
+    rueducommerce_gpu_search,
+    amazon_gpu_search,
 ]
--- tests/test_retailers_init.py
+++ tests/test_retailers_init.py
@@ -1,4 +1,10 @@
-from retailers import ALL_SEARCH_FUNCTIONS, ALL_RAM_SEARCH_FUNCTIONS, ALL_STORAGE_SEARCH_FUNCTIONS
+from retailers import (
+    ALL_SEARCH_FUNCTIONS,
+    ALL_RAM_SEARCH_FUNCTIONS,
+    ALL_STORAGE_SEARCH_FUNCTIONS,
+    ALL_CPU_SEARCH_FUNCTIONS,
+    ALL_GPU_SEARCH_FUNCTIONS,
+)
 
 
 def test_all_search_functions_lists_seven_callables():
@@ -14,3 +20,13 @@ def test_all_ram_search_functions_lists_seven_callables():
 def test_all_storage_search_functions_lists_seven_callables():
     assert len(ALL_STORAGE_SEARCH_FUNCTIONS) == 7
     assert all(callable(fn) for fn in ALL_STORAGE_SEARCH_FUNCTIONS)
+
+
+def test_all_cpu_search_functions_lists_seven_callables():
+    assert len(ALL_CPU_SEARCH_FUNCTIONS) == 7
+    assert all(callable(fn) for fn in ALL_CPU_SEARCH_FUNCTIONS)
+
+
+def test_all_gpu_search_functions_lists_seven_callables():
+    assert len(ALL_GPU_SEARCH_FUNCTIONS) == 7
+    assert all(callable(fn) for fn in ALL_GPU_SEARCH_FUNCTIONS)
